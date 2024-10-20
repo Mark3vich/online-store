cb42de158376b01e3df08901b719cd87b8c7712a
Alexandr <mango.2004@yandex.ru>
2024-10-20 13:09:24 +0300

    Блок с брендами готов

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -11,6 +11,10 @@ import Brand4 from '../../assets/brand-4.webp';
 import Brand5 from '../../assets/brand-5.webp';
 import Brand6 from '../../assets/brand-6.webp';
 
+import Serve1 from '../../assets/image-serv-1.webp';
+import Serve2 from '../../assets/image-serv-2.webp';
+import Serve3 from '../../assets/image-serv-3.webp';
+
 class Home extends React.Component {
     render() {
         return (
@@ -28,7 +32,7 @@ class Home extends React.Component {
                 </div>
                 <div className='container'>
                     <h3 className='text-center mb-5 mt-5'>TOP BRANDS</h3>
-                    <div className="row mb-5 align-items-center">
+                    <div className="row align-items-center mb-5">
                         <div className="col brand-column">
                             <img src={Brand1} alt="Brand1" />
                         </div>
@@ -46,6 +50,30 @@ class Home extends React.Component {
                         </div>
                         <div className="col brand-column">
                             <img src={Brand6} alt="Brand6" />
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-between mb-5 w-100'>
+                        <div className='d-flex p-5 flex-grow-1' style={{ backgroundColor: '#0185ce' }}>
+                            <img src={Serve1} alt="Serve1" style={{ height: 50 + '%' }} />
+                            <div className="ms-2">
+                                <h1 className="text-white text-uppercase font-weight-bold mb-24" style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '16px', letterSpacing: '0.7px' }}>MONEY BACK GUARANTEE</h1>
+                                <h3 className="text-white" style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '15px', letterSpacing: '0.7px' }}>Returns & Exchanges</h3>
+                            </div>
+                        </div>
+                        <div className='d-flex p-5 flex-grow-1' style={{ backgroundColor: '#ffffff', borderColor: '#e7e8e9', borderWidth: '1px', borderStyle: 'solid', marginLeft: '20px', marginRight: '20px' }}>
+                            <img src={Serve2} alt="Serve2" style={{ height: 50 + '%' }} />
+                            <div className="ms-2">
+                                <h1 className="text-uppercase font-weight-bold mb-24" style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '16px', letterSpacing: '0.7px', color: '#171a25' }}>FREE SHIPPING</h1>
+                                <h3 style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '15px', letterSpacing: '0.7px', color: '#757b91' }}>On orders over $99. <br />This offer is valid on all our store items.</h3>
+                            </div>
+                        </div>
+                        <div className='d-flex p-5 flex-grow-1' style={{ backgroundColor: '#171a25' }}>
+                            <img src={Serve3} alt="Serve3" style={{ height: 50 + '%' }} />
+                            <div className="ms-2">
+                                <h1 className="text-white text-uppercase font-weight-bold mb-24" style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '16px', letterSpacing: '0.7px', color: '#171a25' }}>3800-2345-6789</h1>
+                                <h3 style={{ fontFamily: 'Oswald', fontStyle: 'sans-serif', fontSize: '15px', letterSpacing: '0.7px', color: '#757b91' }}>Customer Support
+                                    <br />Days a week from 9:00 am to 7:00 pm</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
